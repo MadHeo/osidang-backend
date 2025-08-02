5107ef8305deaa737f725377b811d620f1b4fa11
MadHeo <tnlfl288@gmail.com>
2025-08-02 15:31:07 +0900

    회원탈퇴 로그 또 추가

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -17,7 +17,11 @@ const authenticateToken = (
 ) => {
   // Authorization 헤더에서 토큰 추출
   const authHeader = req.headers['authorization'];
+  console.log('Authorization 헤더:', authHeader); // 헤더 확인
+
   const token = authHeader && authHeader.split(' ')[1];
+
+  console.log('추출된 토큰:', token); // 토큰 확인
 
   // 토큰이 없는 경우 401 Unauthorized 응답
   if (token == null) return res.sendStatus(401);
@@ -25,7 +29,14 @@ const authenticateToken = (
   // JWT 토큰 검증
   jwt.verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
     // 토큰이 유효하지 않은 경우 403 Forbidden 응답
-    if (err) return res.sendStatus(403);
+    if (err) {
+      console.error('토큰 검증 실패:', {
+        error: err,
+        errorMessage: err.message,
+      });
+      return res.sendStatus(403);
+    }
+    console.log('토큰 정보:', user); // 디코드된 정보 확인
     // 검증된 사용자 정보를 요청 객체에 저장
     req.user = user;
     // 다음 미들웨어로 진행
