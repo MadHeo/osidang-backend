65d6d5b724d9cd09ead3e90d59d956500021d1c5
MadHeo <tnlfl288@gmail.com>
2025-08-04 23:18:10 +0900

    파이어베이스 연결 수정

--- src/middlewares/fireabase.ts
+++ src/middlewares/fireabase.ts
@@ -1,10 +1,11 @@
 import * as admin from 'firebase-admin';
+import 'dotenv/config';
 
 const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -17,7 +18,7 @@ const serviceAccount = {
 // Firebase Admin 초기화
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  storageBucket: 'osidang-c9c8e.appspot.com',
 });
 
 export const bucket = admin.storage().bucket();
